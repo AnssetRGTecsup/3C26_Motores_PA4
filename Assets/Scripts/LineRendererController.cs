cdbba23b26e9b5c947490db7f4eaa3479d6fff5f
imagozxd <144135582+imagozxd@users.noreply.github.com>
2023-12-06 01:12:10 -0500

    Noche de paz
    line espiral

--- Assets/Scripts/LineRendererController.cs
+++ Assets/Scripts/LineRendererController.cs
@@ -8,6 +8,8 @@ public class LineRendererController : MonoBehaviour
     [SerializeField] private float radius;
     [SerializeField] private float lapsAmount;
     [SerializeField] private float angleStep;
+    [SerializeField] private float altura;
+
 
     [Header("Line Render")]
     [SerializeField] private LineRenderer lineRenderer;
@@ -16,20 +18,49 @@ public class LineRendererController : MonoBehaviour
     [SerializeField] private float radiusStep;
     [SerializeField] private int _pivotPoints;
 
+    
     private void Start()
     {
-        _pivotPoints = (int) (360f / angleStep * lapsAmount);
-
+        _pivotPoints = (int)(360f / angleStep * lapsAmount);
         radiusStep = radius / lapsAmount / (360f / angleStep);
-
         angleStep = angleStep * Mathf.Deg2Rad;
-
         lineRenderer.positionCount = _pivotPoints;
 
         for (int i = 0; i < _pivotPoints; i++)
         {
-            Vector3 position = new Vector3(0f, 0f, 0f);
+            float angle = i * angleStep;
+            float x = Mathf.Cos(angle) * radius;
+            float y = i * altura;  // 0.01 funca bien xd
+            float z = Mathf.Sin(angle) * radius;
+            Vector3 position = new Vector3(x, y, z);
             lineRenderer.SetPosition(i, position);
+
+            // reducir radio 
+            radius -= radiusStep;
         }
     }
+    //private void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        DibujandoLine();
+    //    }
+    //}
+    //void DibujandoLine()
+    //{
+    //    _pivotPoints = (int)(360f / angleStep * lapsAmount);
+    //    radiusStep = radius / lapsAmount / (360f / angleStep);
+    //    angleStep = angleStep * Mathf.Deg2Rad;
+    //    lineRenderer.positionCount = _pivotPoints;
+
+    //    for (int i = 0; i < _pivotPoints; i++)
+    //    {
+    //        float angle = i * angleStep;
+    //        float x = Mathf.Cos(angle) * radius;
+    //        float y = i * radiusStep;
+    //        float z = Mathf.Sin(angle) * radius;
+    //        Vector3 position = new Vector3(x, y, z);
+    //        lineRenderer.SetPosition(i, position);
+    //    }
+    //}
 }
